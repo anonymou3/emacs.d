f36dfcb1910b967c39de8cbd6c8f824b05f4dd7d
nr.sila <cocodog@126.com>
2016-10-26 13:55:39 +0800

    Fix error: "X windows are not in use or not initialized"

--- lisp/init-font-set.el
+++ lisp/init-font-set.el
@@ -1,6 +1,16 @@
-(require 'font-set)
-(qiang-set-font
- '("Consolas" "Monaco" "DejaVu Sans Mono" "Monospace" "Courier New") ":pixelsize=18"
- '("Microsoft Yahei" "文泉驿等宽微米黑" "黑体" "新宋体" "宋体"))
+;; Fix error: "X windows are not in use or not initialized"
+;; Ref https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=4000
+
+(if (not (x-display-color-p)) ; if-part
+    (progn
+      (set-face-attribute 'default nil :family "Consolas")
+      (set-face-attribute 'default nil :height 135)
+      ) ; then-part
+  (progn
+    (require 'font-set)
+    (qiang-set-font
+     '("Consolas" "Monaco" "DejaVu Sans Mono" "Monospace" "Courier New") ":pixelsize=18"
+     '("Microsoft Yahei" "文泉驿等宽微米黑" "黑体" "新宋体" "宋体"))
+    ))   ; else-part
 
 (provide 'init-font-set)
