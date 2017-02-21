b7009e02a86fbf5c1e4e9ccd660aba11d5061e44
nr.sila <cocodog@126.com>
2017-02-21 17:05:17 +0800

    elpa source modify.

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -1,10 +1,5 @@
 (require 'package)
 
-;; You can set it to `t' to use safer HTTPS to download packages
-(defvar melpa-use-https-repo nil
-  "By default, HTTP is used to download packages.
-But you may use safer HTTPS instead.")
-
 ;; List of VISIBLE packages from melpa-unstable (http://melpa.org)
 ;; Feel free to add more packages!
 (defvar melpa-include-packages
@@ -16,31 +11,15 @@ But you may use safer HTTPS instead.")
 ;; We include the org repository for completeness, but don't use it.
 ;; Lock org-mode temporarily:
 ;; (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
-(if melpa-use-https-repo
-    (setq package-archives
-          '(;; uncomment below line if you need use GNU ELPA
-            ;; ("gnu" . "https://elpa.gnu.org/packages/")
-           ; ("melpa-cn" . "https://elpa.emacs-china.org/melpa/")
-           ; ("melpa-stable-cn" . "https://elpa.emacs-china.org/melpa-stable/")
-            ("melpa" . "https://melpa.org/packages/")
-            ("melpa-stable" . "https://stable.melpa.org/packages/")))
-  (setq package-archives
-        '(;; uncomment below line if you need use GNU ELPA
-          ;; ("gnu" . "http://elpa.gnu.org/packages/")
-         ; ("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
-         ; ("melpa-stable-cn" . "http://elpa.emacs-china.org/melpa-stable/")
-          ("melpa" . "http://melpa.org/packages/")
-          ("melpa-stable" . "http://stable.melpa.org/packages/")))
-  )
-
-
-;; Un-comment below line if your extract https://github.com/redguardtoo/myelpa/archive/master.zip into ~/myelpa/
-;; (setq package-archives '(("myelpa" . "~/myelpa")))
-
-;; Or Un-comment below line if you install package from https://github.com/redguardtoo/myelpa/
-;; (setq package-archives '(("myelpa" . "https://raw.github.com/redguardtoo/myelpa/master/")))
-
-
+(setq package-archives
+      '(;; uncomment below line if you need use GNU ELPA
+        ;; ("gnu" . "https://elpa.gnu.org/packages/")
+        ;; ("melpa-cn" . "https://elpa.emacs-china.org/melpa/")
+        ;; ("melpa-stable-cn" . "http://elpa.emacs-china.org/melpa-stable/")
+        ;; ("marmalade" . "https://marmalade-repo.org/packages/")
+        ("elpy" . "https://jorgenschaefer.github.io/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("melpa-stable" . "http://stable.melpa.org/packages/")))
 
 ;;------------------------------------------------------------------------------
 ;; Internal implementation, newbies should NOT touch code below this line!
